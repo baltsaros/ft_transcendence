514ca92a73fd2202001bf4ac88b94d55d0dcb7bb
hdony640 <hadriendony@gmail.com>
2023-07-27 17:22:44 +0200

    small test to display a chat page

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,3 +1,4 @@
+/* Entry point of our nestjs appliation */
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
---
+++ apps/frontend/src/pages/Chat.tsx
@@ -0,0 +1,5 @@
+/* The variable Chat gets assigned the value of an arrow function
+** The arrow function is a functional component (= javascript function) of React 
+** Components are used to encapsulate part of the UI to be, here a javascript function */
+const Chat = () => <h1>Chat!</h1>
+export default Chat
