07e408d87cd134c1c9e63699eba52b93d06f6554
hdony640 <hadriendony@gmail.com>
2023-07-28 12:18:45 +0200

    SimpleChat UI

--- apps/frontend/src/pages/Chat.tsx
+++ apps/frontend/src/pages/Chat.tsx
@@ -1,5 +1,13 @@
 /* The variable Chat gets assigned the value of an arrow function
 ** The arrow function is a functional component (= javascript function) of React 
 ** Components are used to encapsulate part of the UI to be, here a javascript function */
-const Chat = () => <h1>Chat!</h1>
-export default Chat
+function SimpleChat() {
+    // state, données dynamiques
+
+    // behavior
+
+    // render (jsx)
+    return <h1>Bonjour Hadri</h1>
+}
+
+export default SimpleChat;
