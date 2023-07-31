b536d69b54142275d30013de0d860eee6776737f
hdony640 <hadriendony@gmail.com>
2023-07-31 11:39:32 +0200

    Input text solved

--- apps/frontend/src/components/ChatBar.tsx
+++ apps/frontend/src/components/ChatBar.tsx
@@ -2,29 +2,34 @@ import { useState } from "react";
 
 function ChatBar() {
     // state
-    const [sample, setSample] =  useState("");
+    const [sample, setSample] =  useState("Sample");
 
     // behavior
     // const handleClick = () => {
     //     alert("handleClick");
     // }
 
+    const handleSubmit = (event: React.ChangeEvent<HTMLInputElement>) => {
+        event.preventDefault();
+    }
+
     const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        const valueAfterChange = event.target.value;
-        console.log(valueAfterChange);
+        console.log(event.target.value);
         setSample(event.target.value);
     }
 
     // render
     return (
-        <div className="flex items-center bg-gray-200 p-2">
-            <input
-            value={sample}
-            type="text" 
-            placeholder="Type your message..."
-            onChange={handleChange}
-            className="flex-grow p-2 border rounded-l-lg"
-            />
+        <div className="flex text-black items-center bg-gray-200 p-2">
+            {/* <form action="submit" onSubmit={handleSubmit}> */}
+                <input
+                value={sample}
+                type="text" 
+                placeholder="Type your message..."
+                onChange={handleChange}
+                className="flex-grow p-2 border rounded-l-lg"
+                />
+            {/* </form> */}
             <button className="bg-blue-500 text-white p-3 rounded-r-lg">Send</button>
         </div>
     );
