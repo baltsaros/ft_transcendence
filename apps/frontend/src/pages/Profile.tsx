533ed4e50c05c7df9cdb1bce9f5cef40d1ce38f6
baltsaros <buzdin.alexander@gmail.com>
2023-08-11 10:57:36 +0200

    upd for styling; upd for profile page

--- apps/frontend/src/pages/Profile.tsx
+++ apps/frontend/src/pages/Profile.tsx
@@ -4,24 +4,29 @@ import { FC, useState } from "react";
 import { AuthService } from "../services/auth.service";
 import { toast } from "react-toastify";
 import { useAppDispatch } from "../store/hooks";
-import { useNavigate } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
 import { login } from "../store/user/userSlice";
 
 const Profile: FC = () => {
   const user = useAppSelector((state: RootState) => state.user.user);
   const [username, setUsername] = useState<string>("");
+  const [avatar, setAvatar] = useState<string>("");
   const dispatch = useAppDispatch();
   const navigate = useNavigate();
-  const nameUpdateHandler = async (e: React.FormEvent<HTMLFormElement>) => {
+  const updateHandler = async (e: React.FormEvent<HTMLFormElement>) => {
     try {
       e.preventDefault();
-      if (username == user?.username) {
+      if (username == "") {
+        toast.error("Username field cannot be empty");
+        return;
+      }
+      else if (username == user?.username) {
         toast.error("Username cannot be the same");
         return;
       }
       const tmp = {
         username: username,
-        avatar: user ? user.avatar : "",
+        avatar: avatar.length > 0 ? avatar : (user ? user.avatar : ""),
         id: user ? user.id : 0,
         intraId: user ? user.intraId : 0,
         intraToken: user ? user.intraToken : "",
@@ -30,9 +35,7 @@ const Profile: FC = () => {
       console.log(tmp);
       const data = await AuthService.update(tmp);
       if (data) {
-        toast.success("Username was successfully update!");
-        // console.log(data.username);
-        dispatch(login(data));
+        toast.success("User information was successfully updated!");
         navigate("/");
         window.location.reload();
       }
@@ -42,39 +45,56 @@ const Profile: FC = () => {
     }
   };
 
-  // const loginHandler = async (e: React.FormEvent<HTMLFormElement>) => {
-  //   try {
-  //     e.preventDefault();
-  //     const data = await AuthService.login({ username, email, password });
-  //     if (data) {
-  //       setTokenToLocalStorage("token", data.access_token);
-  //       dispatch(login(data));
-  //       toast.success("Access granted ;)");
-  //       navigate("/");
-  //     }
-  //   } catch (err: any) {
-  //     const error = err.response?.data.message;
-  //     toast.error(error.toString());
-  //   }
-  // };
+  const uploadImage = async (e: React.FormEvent<HTMLFormElement>) => {
+      e.preventDefault();
+      if (!avatar) return ;
+      toast.success("File was successfully added!");
+      // setAvatar(e.target.value);
+  };
 
   return (
-    <div>
-      <form onSubmit={nameUpdateHandler}>
-        <input
-          type="text"
-          className="input"
-          placeholder="Username"
-          onChange={(e) => setUsername(e.target.value)}
-        />
+    <div className="mt-8 w-3/5 mx-auto bg-gray-500 text-black uppercase">
+      <h2 className="bg-cyan-300 w-full mb-2 p-5 tracking-wider text-lg">
+        Profile creation
+      </h2>
+      <h2 className="bg-gray-500 w-full mb-4 p-5 tracking-wider"></h2>
+      <div className="bg-cyan-300 flex justify-start mb-2 p-5 space-x-4 items-center">
+        <h3 className="pl-2 pr-6">Enter username:</h3>
+        <form className="">
+          <input
+            type="text"
+            className="input"
+            // placeholder="Username"
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </form>
         {/* <input
-          type="password"
-          className="input"
-          placeholder="Password"
-          onChange={(e) => setPassword(e.target.value)}
-        /> */}
-        <button className="btn btn-green mx-auto">Submit</button>
-      </form>
+              type="password"
+              className="input"
+              placeholder="Password"
+              onChange={(e) => setPassword(e.target.value)}
+            /> */}
+      </div>
+      <div className="bg-gray-500 flex flex-col justify-center mb-4 items-center">
+        {user?.avatar ? (
+          <img
+            src={user.avatar}
+            alt="ImageError"
+            className="rounded-full h-16 w-16 overflow-hidden mt-2"
+          />
+        ) : (
+          "AVATAR"
+        )}
+        <div className="bg-cyan-300 flex w-full justify-center p-2 mt-4">Upload avatar</div>
+      </div>
+      <div className="flex flex-row items-end justify-between">
+        <form onSubmit={updateHandler}>
+          <button className="btn btn-gray">Ok</button>
+        </form>
+        <NavLink to={"/"}>
+          <button className="btn btn-gray">Cancel</button>
+        </NavLink>
+      </div>
     </div>
   );
 };
