f5e2451ffdfabedbf37b412f8e64530d682612bc
Elsa Joo-Thomson <elsajoo-thomson@MBP-de-Elsa.cust.communityfibre.co.uk>
2023-09-15 16:22:13 +0100

    PlayersOnServer has one list for online players and one for offline players

--- apps/frontend/src/components/chat/PlayersOnServer.tsx
+++ apps/frontend/src/components/chat/PlayersOnServer.tsx
@@ -43,15 +43,28 @@ function PlayersOnServer() {
         <div className="flex flex-col items-stretch justify-center h-screen bg-gray-100 w-full">
             <div className="flex flex-grow w-full">
                 <div className="flex-1 p-4 border bg-gray-100 m-2">
-                    <div className="flex-1 p-4 border bg-gray-100 m-2">
+                    <div className="flex-shrink-0 p-4 border bg-gray-100 m-2">
                         <h1 className="text-lg font-bold mb-2 text-gray-600">Players on server</h1>
-                        <div className="flex flex-col text-black space-y-4">
-                            {players.map(player => (
-                                <div className="bg-blue-300 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded" key={player.id} >
-                                    <button>{ player.username }</button>
-                                </div>
-                            ))}
-                        </div>
+                    </div>
+                    <div className="flex-shrink-0 p-4 bg-gray-100 m-2">
+                        <p className="text-base mb-1 text-gray-600">Online</p>
+                    </div>
+                    <div className="flex flex-col text-black space-y-4">
+                        {players.map(player => (
+                            player.status === 'online' && <div className="bg-blue-300 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded" key={player.id} >
+                                <button>{ player.username }</button>
+                            </div>
+                        ))}
+                    </div>
+                    <div className="flex-shrink-0 p-4 bg-gray-100 m-2">
+                        <p className="text-base mb-1 text-gray-600">Offline</p>
+                    </div>
+                    <div className="flex flex-col text-black space-y-4">
+                        {players.map(player => (
+                            player.status === 'offline' && <div className="bg-blue-300 hover:bg-blue-600 text-white font-semibold py-2 px-4 rounded" key={player.id} >
+                                <button>{ player.username }</button>
+                            </div>
+                        ))}
                     </div>
                 </div>
             </div>
