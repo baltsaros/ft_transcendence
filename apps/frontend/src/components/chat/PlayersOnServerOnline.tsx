8fc92cbdcd224b25b9d0a0757b0582301263c7c7
Elsa Joo-Thomson <elsajoo-thomson@MacBook-Pro-de-Elsa.local>
2023-09-18 20:48:34 +0200

    in PlayersOnServerOnline the dropdown menu button is closed when one clicks anywhere on the screen

--- apps/frontend/src/components/chat/PlayersOnServerOnline.tsx
+++ apps/frontend/src/components/chat/PlayersOnServerOnline.tsx
@@ -1,7 +1,9 @@
-import { useState } from 'react';
+import { useState, useEffect, useRef } from 'react';
 
 const DropdownButton = ({ username }) => {
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const dropdownButtonRef = useRef(null);
+  const dropdownMenuRef = useRef(null);
 
   const toggleDropdown = () => {
     setIsDropdownOpen(!isDropdownOpen);
@@ -13,16 +15,43 @@ const DropdownButton = ({ username }) => {
     // You can add more logic here based on the clicked item
   };
 
+  useEffect(() => {
+    const closeDropdownOnOutsideClick = (event) => {
+      // Check if the click occurred outside the button and the dropdown menu
+      if (
+        dropdownButtonRef.current &&
+        !dropdownButtonRef.current.contains(event.target) &&
+        dropdownMenuRef.current &&
+        !dropdownMenuRef.current.contains(event.target)
+      ) {
+        // Click occurred outside, so close the dropdown
+        setIsDropdownOpen(false);
+      }
+    };
+
+    // Add the global click event listener
+    document.addEventListener('click', closeDropdownOnOutsideClick);
+
+    // Remove the event listener when the component unmounts
+    return () => {
+      document.removeEventListener('click', closeDropdownOnOutsideClick);
+    };
+  }, []);
+
   return (
     <div className="relative inline-block text-left">
       <button
         onClick={toggleDropdown}
+		ref={dropdownButtonRef}
         className="px-4 py-2 text-white bg-blue-500 rounded-md hover:bg-blue-600 focus:outline-none"
       >
         {username}
       </button>
       {isDropdownOpen && (
-        <div className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 z-10">
+        <div 
+			ref={dropdownMenuRef}
+			className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 z-10"
+		>
           {/* Dropdown menu items */}
           <div className="py-1">
             <button
