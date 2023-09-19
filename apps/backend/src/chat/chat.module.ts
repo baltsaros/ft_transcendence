f980dd633c86807b1a1f521e1b38bfb86fc211b9
hdony640 <hadriendony@gmail.com>
2023-09-19 14:28:17 +0200

    Double WS instances solved

--- apps/backend/src/chat/chat.module.ts
+++ apps/backend/src/chat/chat.module.ts
@@ -1,5 +1,4 @@
 import { Module } from "@nestjs/common";
-import { ChatGateway } from "./chat.gateway";
 import { ChatService } from "./chat.service";
 import { TypeOrmModule } from '@nestjs/typeorm'
 import { Channel } from "src/channel/channel.entity";
@@ -7,7 +6,7 @@ import { Channel } from "src/channel/channel.entity";
 @Module({
     imports: [TypeOrmModule.forFeature([Channel])],
     controllers: [],
-    providers: [ChatGateway, ChatService],
+    providers: [ChatService],
     exports: [ChatService],
 })
 
