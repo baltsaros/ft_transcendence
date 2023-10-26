d4ca8cb2f52b3ed24cd94d9ef0604d01f82d4adc
Aleksandr Buzdin <abuzdin@shi-r6-p4.s19.be>
2023-10-26 10:35:33 +0200

    merge with main

--- apps/frontend/src/context/chat.websocket.context.tsx
+++ apps/frontend/src/context/chat.websocket.context.tsx
@@ -18,7 +18,6 @@ export const ChatWebSocketProvider: React.FC<{ children: React.ReactNode }> = ({
   const [ChatwebSocketService, setChatWebSocketService] = useState<ChatWebSocketService | undefined>();
   useEffect(() => {
       const user = Cookies.get("username");
-      console.log('user', user);
       if (user) {
         setChatWebSocketService(new ChatWebSocketService(user));
       }
