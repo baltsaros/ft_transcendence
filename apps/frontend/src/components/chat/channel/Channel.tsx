5a4bacff087a596a25c1a10e0ea37424a9db6416
hdony640 <hadriendony@gmail.com>
2023-10-29 10:23:31 +0100

    minor changes

--- apps/frontend/src/components/chat/channel/Channel.tsx
+++ apps/frontend/src/components/chat/channel/Channel.tsx
@@ -70,7 +70,7 @@ const Channels: React.FC<ChildProps> = ({onSelectChannel}) => {
                                         <div className="flex items-center bg-gray-500 rounded-lg">
                                         	<button
                                         		onClick={() => onSelectChannel(channel)}
-                                        		className="bg-gray-500 hover:bg-gray-600 text-white p-3 ">{channel.name}
+                                        		className="bg-gray-500 hover:bg-gray-600 text-white p-3 rounded-lg">{channel.name}
                                         	</button>
                                         	<ChannelMenu channel={channel}/>
                                         </div>
